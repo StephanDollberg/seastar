43cb5622b1fc4441d028e858dae37df8602723c0
Stephan Dollberg <stephan@redpanda.com>
2023-04-24 13:48:30 +0100

    memory: Use separate small pool for tracking sampled allocations
    To differentiate sampled from non-sampled allocations we use a separate
    set of small pools.
    
    Each pool gets tagged to indicate whether it's for sampled allocations
    or not.
    
    Hence, on free we can simply check whether an allocation belongs to a
    sampled-pool or not and act accordingly.
    
    For sampled pools we could reduce the amount of target free objects in a
    pool but for any serious workload this probably doesn't matter.
    
    Large allocations continue to use the `allocation_site` pointer on the
    `page` structure. However, we now always need to check whether the
    pointer is valid as it's null if not sampled.

--- tests/unit/alloc_test.cc
+++ tests/unit/alloc_test.cc
@@ -331,35 +331,125 @@ SEASTAR_TEST_CASE(test_diagnostics_allocation) {
 
 #ifdef SEASTAR_HEAPPROF
 
-SEASTAR_TEST_CASE(test_sampled_profile_collection)
+// small wrapper to disincentivize gcc from unrolling the loop
+[[gnu::noinline]]
+char* malloc_wrapper(size_t size) {
+    auto ret = static_cast<char*>(malloc(size));
+    *ret = 'c'; // to prevent compiler from considering this a dead allocation and optimizing it out
+    return ret;
+}
+
+SEASTAR_TEST_CASE(test_sampled_profile_collection_small)
 {
-    BOOST_REQUIRE(!seastar::memory::get_heap_profiling_enabled());
+    {
+        auto stats = seastar::memory::memory_profile();
+        BOOST_REQUIRE_EQUAL(stats.size(), 0);
+    }
+
+    std::size_t count = 100;
+    std::vector<volatile char*> ptrs(count);
+
     seastar::memory::set_heap_profiling_enabled(true);
-    BOOST_REQUIRE(seastar::memory::get_heap_profiling_enabled());
+
+#ifdef __clang__
+    #pragma nounroll
+#endif
+    for (std::size_t i = 0; i < count / 2; ++i)
+    {
+        ptrs[i] = malloc_wrapper(10);
+    }
+
+#ifdef __clang__
+    #pragma nounroll
+#endif
+    for (std::size_t i = count / 2; i < count; ++i)
+    {
+        ptrs[i] = malloc_wrapper(10);
+    }
+
+    // NB: the test framework allocates
+    seastar::memory::set_heap_profiling_enabled(0);
 
     {
         auto stats = seastar::memory::memory_profile();
-        BOOST_REQUIRE_EQUAL(stats.size(), 0);
+
+        for (const auto& stat : stats)
+        {
+            std::cout << stat.backtrace << std::endl;
+        }
+
+        BOOST_REQUIRE_EQUAL(stats.size(), 2);
+        BOOST_REQUIRE_EQUAL(stats[0].size, stats[0].count * 100);
     }
 
-    volatile char* ptr = static_cast<char*>(malloc(10));
-    *ptr = 'c'; // to prevent compiler from considering this a dead allocation and optimizing it out
-
-    {
-        auto stats = seastar::memory::memory_profile();
-        BOOST_REQUIRE_EQUAL(stats.size(), 1);
-        BOOST_REQUIRE_EQUAL(stats[0].size, 32); // 10 + 8 falls into 32 byte pool
-    }
-
-    free((void*)ptr);
-
-    {
-        auto stats = seastar::memory::memory_profile();
-        BOOST_REQUIRE_EQUAL(stats.size(), 0);
-    }
-
-    // Needed for now because we can't differentiate between sampled allocations and non-sampled ones
     seastar::memory::set_heap_profiling_enabled(false);
+
+    for (auto ptr : ptrs)
+    {
+        free((void*)ptr);
+    }
+
+    seastar::memory::set_heap_profiling_enabled(0);
+
+    {
+        auto stats = seastar::memory::memory_profile();
+        BOOST_REQUIRE_EQUAL(stats.size(), 0);
+    }
+
+    return seastar::make_ready_future();
+}
+
+SEASTAR_TEST_CASE(test_sampled_profile_collection_large)
+{
+    {
+        auto stats = seastar::memory::memory_profile();
+        BOOST_REQUIRE_EQUAL(stats.size(), 0);
+    }
+
+    std::size_t count = 100;
+    std::vector<volatile char*> ptrs(count);
+
+    seastar::memory::set_heap_profiling_enabled(1000000);
+
+#ifdef __clang__
+    #pragma nounroll
+#endif
+    for (std::size_t i = 0; i < count / 2; ++i)
+    {
+        ptrs[i] = malloc_wrapper(100000);
+    }
+
+#ifdef __clang__
+    #pragma nounroll
+#endif
+    for (std::size_t i = count / 2; i < count; ++i)
+    {
+        ptrs[i] = malloc_wrapper(100000);
+    }
+
+    // NB: the test framework allocate
+    seastar::memory::set_heap_profiling_enabled(0);
+
+    {
+        auto stats = seastar::memory::memory_profile();
+        BOOST_REQUIRE_EQUAL(stats.size(), 2);
+        BOOST_REQUIRE_EQUAL(stats[0].size, stats[0].count * 1000000);
+    }
+
+    seastar::memory::set_heap_profiling_enabled(1000000);
+
+    for (auto ptr : ptrs)
+    {
+        free((void*)ptr);
+    }
+
+    seastar::memory::set_heap_profiling_enabled(0);
+
+    {
+        auto stats = seastar::memory::memory_profile();
+        // NOTE this is because right now the tracking structure doesn't delete call sites ever
+        BOOST_REQUIRE_EQUAL(stats.size(), 0);
+    }
 
     return seastar::make_ready_future();
 }
